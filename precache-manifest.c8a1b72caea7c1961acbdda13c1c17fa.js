d693b9a9114b23f8333dc4d3feaf25cab0649976
javier <jav8319@hotmail.com>
2023-02-26 14:30:42 -0500

    Updates

---
+++ precache-manifest.c8a1b72caea7c1961acbdda13c1c17fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37daef90b4c989ef3c959c7979126911",
+    "url": "/kanbas-board/index.html"
+  },
+  {
+    "revision": "1320a494180f8ac07315",
+    "url": "/kanbas-board/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "670b20161137fa9c8fa1",
+    "url": "/kanbas-board/static/css/main.84c7cbd4.chunk.css"
+  },
+  {
+    "revision": "1320a494180f8ac07315",
+    "url": "/kanbas-board/static/js/2.591a7d6e.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/kanbas-board/static/js/2.591a7d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670b20161137fa9c8fa1",
+    "url": "/kanbas-board/static/js/main.07003d0b.chunk.js"
+  },
+  {
+    "revision": "2a847de6ff1e7e8da728",
+    "url": "/kanbas-board/static/js/runtime-main.3e7d6ccc.js"
+  }
+]);
